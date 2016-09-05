e7cd791ce517fe7292806658e696593ab17f86d6
buptxge <buptxge@users.noreply.github.com>
2016-09-05 20:46:28 +0800

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.py
+++ 15-3Sum/solution.py
@@ -4,29 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-    sum_3 = 0
-    nums.sort()
-    l = len(nums)
-    result = []
-    for i in range(l-2):
-        first = nums[i]
-        if (i > 0) and (nums[i-1] == first):
-            continue
-        left = i + 1
-        right = l - 1
-        sum_2 = sum_3 - first
-        while(left < right):
-            current_sum = nums[left] + nums[right]
-            if (current_sum == sum_2):
-                result.append([first, nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while (left < right and nums[left-1] == nums[left]):
+        sum_3 = 0
+        nums.sort()
+        l = len(nums)
+        result = []
+        for i in range(l-2):
+            first = nums[i]
+            if (i > 0) and (nums[i-1] == first):
+                continue
+            left = i + 1
+            right = l - 1
+            sum_2 = sum_3 - first
+            while(left < right):
+                current_sum = nums[left] + nums[right]
+                if (current_sum == sum_2):
+                    result.append([first, nums[left], nums[right]])
                     left += 1
-                while (left < right and nums[right+1] == nums[right]):
                     right -= 1
-            elif current_sum > sum_2:
-                right -= 1
-            else:
-                left += 1
-    return result
+                    while (left < right and nums[left-1] == nums[left]):
+                        left += 1
+                    while (left < right and nums[right+1] == nums[right]):
+                        right -= 1
+                elif current_sum > sum_2:
+                    right -= 1
+                else:
+                    left += 1
+        return result
